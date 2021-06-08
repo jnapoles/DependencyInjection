f0daf51b8dab5aac33daf3db86af5ab7db15d68c
Jorge Nápoles <jnapoles@gmail.com>
2021-06-07 23:50:47 -0500

    controlling scope and lifetime

--- ControllingScopeLifeTime/Program.cs
+++ ControllingScopeLifeTime/Program.cs
@@ -11,17 +11,20 @@ namespace ControllingScopeLifeTime
     {
         void Write(string message);
     }
-
     public interface IConsole
     {
 
     }
-
-    public class ConsoleLog : ILog, IConsole
+    public class ConsoleLog : ILog, IConsole,IDisposable
     {
         public ConsoleLog()
         {
             Console.WriteLine("Creating a console log!");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Destroyed a console log!");
         }
 
         public void Write(string message)
@@ -29,7 +32,6 @@ namespace ControllingScopeLifeTime
             Console.WriteLine(message);
         }
     }
-
     public class EmailLog : ILog
     {
         private const string adminEmail = "admin@foo.com";
@@ -39,7 +41,38 @@ namespace ControllingScopeLifeTime
             Console.WriteLine($"Email sent to {adminEmail} : {message}");
         }
     }
+    public interface IResource
+    {
 
+    }
+    class SingletonResource : IResource
+    {
+    }
+    public class InstancePerDependencyResource : IResource, IDisposable
+    {
+        public InstancePerDependencyResource()
+        {
+            Console.WriteLine("Instance per dep created");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Instance per dep destroyed");
+        }
+    }
+    public class ResourceManager
+    {
+        public ResourceManager(IEnumerable<IResource> resources)
+        {
+            if (resources == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(resources));
+            }
+            Resources = resources;
+        }
+
+        public IEnumerable<IResource> Resources { get; set; }
+    }
     public class Engine
     {
         private ILog log;
@@ -62,7 +95,6 @@ namespace ControllingScopeLifeTime
             log.Write($"Engine [{id}] ahead {power}");
         }
     }
-
     public class SMSLog : ILog
     {
         private string phoneNumber;
@@ -77,7 +109,6 @@ namespace ControllingScopeLifeTime
             Console.WriteLine($"SMS to {phoneNumber} : {message}");
         }
     }
-
     public class Car
     {
         private Engine engine;
@@ -101,7 +132,6 @@ namespace ControllingScopeLifeTime
             log.Write("Car going forward...");
         }
     }
-
     public class Parent
     {
         public override string ToString()
@@ -109,24 +139,47 @@ namespace ControllingScopeLifeTime
             return "I am your father";
         }
     }
-
     public class Child
     {
         public string Name { get; set; }
         public Parent Parent { get; set; }
-
+        public Child()
+        {
+            Console.WriteLine("Child being created");
+        }
         public void SetParent(Parent parent)
         {
             Parent = parent;
         }
+        public override string ToString()
+        {
+            return "Hi there";
+        }
     }
-
+    class BadChild : Child
+    {
+        public override string ToString()
+        {
+            return "I hate you";
+        }
+    }
     public class ParentChildModule : Module
     {
         protected override void Load(ContainerBuilder builder)
         {
             builder.RegisterType<Parent>();
             builder.Register(c => new Child() { Parent = c.Resolve<Parent>() });
+        }
+    }
+    public class MyClass : IStartable
+    {
+        public MyClass()
+        {
+            Console.WriteLine("MyClass is created!!!!");
+        }
+        public void Start()
+        {
+            Console.WriteLine("MyClass is start!!!!!");
         }
     }
     class Program
@@ -210,7 +263,107 @@ namespace ControllingScopeLifeTime
 
 
 
+            // Captive Dependencies
+            //var builder = new ContainerBuilder();
+            //builder.RegisterType<ResourceManager>().SingleInstance();
+            //builder.RegisterType<SingletonResource>().As<IResource>().SingleInstance();
+            //builder.RegisterType<InstancePerDependencyResource>().As<IResource>();
 
+
+
+            //using (var container = builder.Build())
+            //{
+            //    using (var scope = container.BeginLifetimeScope())
+            //    {
+            //        scope.Resolve<ResourceManager>(); 
+            //    }
+
+            //    using (var scope2 = container.BeginLifetimeScope())
+            //    {
+            //        scope2.Resolve<ResourceManager>();
+            //    }
+            //}
+
+
+            //Disposable
+            //var builder = new ContainerBuilder();
+            ////builder.RegisterType<ConsoleLog>();
+            //builder.RegisterInstance(new ConsoleLog());
+            ////var container = builder.Build();
+            //using (var container = builder.Build())
+            //{
+            //    using (var scope = container.BeginLifetimeScope())
+            //    {
+            //        scope.Resolve<ConsoleLog>();
+            //    }
+            //}
+
+
+
+            // LifeTime Scope
+            //var builder = new ContainerBuilder();
+            //builder.RegisterType<Parent>();
+            //builder.RegisterType<Child>()
+            //    .OnActivating(c =>
+            //    {
+            //        Console.WriteLine("Child Activating!!");
+            //        //c.Instance.Parent = c.Context.Resolve<Parent>();
+            //        c.ReplaceInstance(new BadChild());
+            //    }
+            //    )
+            //    .OnActivated(c=>
+            //    {
+            //        Console.WriteLine("Child Activated");
+            //        //c.Instance.Parent = c.Context.Resolve<Parent>();
+            //    }    
+            //    )
+            //    .OnRelease(c=>
+            //    {
+            //        Console.WriteLine("Child destroyed!!!!");
+            //    })
+            //    ;
+
+            ////builder.RegisterType<ConsoleLog>()
+            ////    .As<ILog>()
+            ////    .OnActivating(
+            ////            c =>
+            ////            {
+            ////                c.ReplaceInstance(new SMSLog("+3314714441"));
+            ////            }
+            ////    );
+
+            //builder.RegisterType<ConsoleLog>().AsSelf();
+            //builder.Register<ILog>(c => c.Resolve<ConsoleLog>())
+            //    .OnActivating(c =>
+            //    {
+            //        c.ReplaceInstance(new SMSLog("+3314714441"));
+            //    });
+
+            //using (var container = builder.Build().BeginLifetimeScope())
+            //{
+            //    var child = container.Resolve<Child>();
+            //    var parent = child.Parent;
+            //    Console.WriteLine(child);
+            //    Console.WriteLine(parent);
+
+
+
+            //    var log = container.Resolve<ILog>();
+            //    log.Write("Testing");
+            //}
+
+
+
+
+            // running code of startup
+            var builder = new ContainerBuilder();
+            builder.RegisterType<MyClass>().AsSelf().As<IStartable>().SingleInstance();
+
+
+            var container = builder.Build();
+
+
+            var myclass = container.Resolve<MyClass>();
         }
     }
 }
